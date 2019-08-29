15688c52d69284aa55426c6cb12e714df7a29f6d
Donte Hightower <you@example.com>
2019-08-29 16:40:46 -0500

    update head

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ const Home = () => (
   <div>
     <Head>
       <title>Home</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
       <style jsx global>
         {`
           * {
