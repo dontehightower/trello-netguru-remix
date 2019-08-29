de5a5737c9d1decdbedec12da51fb4d9d9c87429
Donte Hightower <you@example.com>
2019-08-29 17:27:38 -0500

    add Hero & Borads section

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Boards from '../components/Boards';
 import StyledLanding from '../styles/Landing';
 
 const Home = () => (
@@ -10,11 +12,14 @@ const Home = () => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <link
-        href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap"
+        href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap"
         rel="stylesheet"
       />
       <style jsx global>
         {`
+          html {
+            font-size: 62.5%;
+          }
           * {
             box-sizing: border-box;
             margin: 0;
@@ -26,6 +31,8 @@ const Home = () => (
 
     <StyledLanding>
       <Navbar />
+      <Hero />
+      <Boards />
     </StyledLanding>
   </div>
 );
