66963401431bfad94a9e9e7af067143cef0e6b10
Donte Hightower <you@example.com>
2019-08-29 17:49:01 -0500

    create Copy styled-component

---
+++ styles/Copy.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Copy = styled.p`
+  font-family: 'Poppins', sans-serif;
+  font-size: 1.6rem;
+  line-height: 1.5;
+  margin-bottom: 2.4rem;
+  color: #3d4568;
+`;
+
+export default Copy;
