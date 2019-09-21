c78f4febe8907286fea3d73e1b0a790c65fd293b
dontehightower <dontedhightower@gmail.com>
2019-09-20 22:10:22 -0500

    createGlobalStyles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import { createGlobalStyle } from 'styled-components';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Boards from '../components/Boards';
@@ -9,40 +10,43 @@ import ClientSection from '../components/ClientSection';
 import Footer from '../components/Footer';
 import StyledLanding from '../styles/Landing';
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
-      <link
-        href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap"
-        rel="stylesheet"
-      />
-      <style jsx global>
-        {`
-          html {
-            font-size: 62.5%;
-          }
-          * {
-            box-sizing: border-box;
-            margin: 0;
-            padding: 0;
-          }
-        `}
-      </style>
-    </Head>
+const GlobalStyles = createGlobalStyle`
+ html {
+    font-size: 62.5%;
+  }
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
 
-    <StyledLanding>
-      <Navbar />
-      <Hero />
-      <Boards />
-      <Details />
-      <Calendar />
-      <ClientSection />
-      <Footer />
-    </StyledLanding>
-  </div>
+const Home = () => (
+  <React.Fragment>
+    <GlobalStyles />
+    <div>
+      <Head>
+        <title>Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <StyledLanding>
+        <Navbar />
+        <Hero />
+        <Boards />
+        <Details />
+        <Calendar />
+        <ClientSection />
+        <Footer />
+      </StyledLanding>
+    </div>
+  </React.Fragment>
 );
 
 export default Home;
